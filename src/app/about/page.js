960f9067bd0de46ab10554879e3886d4ce39d620
huraira <hurairabaloch996@gmail.com>
2023-07-23 23:19:27 +0500

    NEXT JS Dynamic pages Movies webAPP

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+const About = () => {
+  return (
+    <div>
+      <HeroSection title={"Our Story"} image={"/about1.svg"} />
+    </div>
+  );
+};
+
+export default About;

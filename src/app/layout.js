c973413b538c5a29a9bf6c67a20d60c22e61a9e6
huraira <hurairabaloch996@gmail.com>
2023-07-24 08:01:35 +0500

    chaning layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,13 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lugrasimo&family=Mulish:ital,wght@0,200;0,300;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600&family=Satisfy&family=Tektur:wght@400;500&family=Urbanist:ital,wght@0,100;0,200;0,600;1,100;1,300;1,600;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head></head>
       <body>
         <Header />
 
